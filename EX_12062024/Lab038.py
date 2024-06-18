4f4bf920a6d9f3cd8523d553a189a24d4c5351b8
Shraddha Santoshe <shraddhasantoshe001@gmail.com>
2024-06-18 18:01:49 +0530

    add remaining files

---
+++ EX_12062024/Lab038.py
@@ -0,0 +1,13 @@
+# Conditions and Loop
+#if age is > 18 then allowed to go to Goa Club and if age is < 18 then not allowed
+
+#If Else
+
+
+
+age = int(input("Enter your age"))
+
+if age>18:
+    print("Go to the club")
+else:
+    print("Not allowed")
